552bef310476e241265bec1984411de4ad978f3e
anant0059 <2019uce0059@gmail.com>
2023-02-01 22:39:48 +0530

    server and routes

--- my-express-sever/server.js
+++ my-express-sever/server.js
@@ -9,6 +9,18 @@ app.get("/",function(request, response){
     response.send("<h1>Hello World!!</h1>");
 });
 
+app.get("/contact", function(req, res){
+    res.send("Contact me at: 2019uce0059@gmail.com.");
+});
+
+app.get("/about", function(req, res){
+    res.send("I am final year student at IIT Jammu.");
+});
+
+app.get("/hobbies", function(req,res){
+    res.send("I love reading, taking challenges.")
+});
+
 app.listen(3000, function(){
-    console.log("server started on port 3000");
+    console.log("server started on port 3000.");
 });
