dd0bcbfc8e00454c2fafffb217ee3168ec9c7f0f
anant0059 <2019uce0059@gmail.com>
2023-02-01 18:15:04 +0530

    express-server

---
+++ my-express-sever/server.js
@@ -0,0 +1,14 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const app = express();
+
+app.get("/",function(request, response){
+    console.log(request);
+    response.send("<h1>Hello World!!</h1>");
+});
+
+app.listen(3000, function(){
+    console.log("server started on port 3000");
+});
